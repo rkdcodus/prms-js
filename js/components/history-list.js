059fbea64934b3ae6a860f383358f78fdcb1eb16
rkdcodus <rcd7275@naver.com>
2025-01-06 22:50:06 +0900

    소비내역 목록 보여주기, 정렬, 시간 포맷, 금액 포맷

--- js/components/history-list.js
+++ js/components/history-list.js
@@ -31,11 +31,7 @@ function reRender() {
 }
 
 export function renderHistoryList() {
-  // TODO: 데이터 매핑
-  // TODO: 오름차순으로 목록 나열
-  // TODO: 항목의 시간 포맷 변경: `HH:mm`
-  // TODO: 금액 콤마 포맷 맞추기
-
+  $sectionHistory.appendChild;
   $sectionHistory.innerHTML = store.dateList
     .map(({ date, id: dateId }) => {
       const detail = store.detailList[dateId];
@@ -43,33 +39,43 @@ export function renderHistoryList() {
 
       return `<article class="history-per-day">
       <p class="history-date">2021년 12월 1일</p>
-      <section class="history-item">
+      ${detail
+        .sort((a, b) => b.id - a.id)
+        .map(({ description, category, amount, fundsAtTheTime, createAt, id }) => {
+          const time = new Date(createAt).toLocaleTimeString("ko-kr", {
+            timeStyle: "short",
+            hourCycle: "h24",
+          });
+          return `<section class="history-item">
         <section class="history-item-column">
-          <div class="create-at">10:30</div>
+          <div class="create-at">${time}</div>
           <div class="history-detail">
             <div class="history-detail-row history-detail-title">
-              <p>아이스 아메리카노</p>
+              <p>${description}</p>
             </div>
             <div class="history-detail-row history-detail-subtitle">
-              <p>카페</p>
+              <p>${category}</p>
               <p>
-                1000000
+                ${amount.toLocaleString()}
                 <span>원</span>
               </p>
             </div>
           </div>
           <div class="delete-section">
-            <button class="delete-button">🗑</button>
+            <button class="delete-button" data-dateid=${dateId} data-itemid=${id}>🗑</button>
           </div>
         </section>
         <section class="history-item-caption">
           <p>
             <span>남은 자산</span>
-            <span>300000</span>
+            <span>${fundsAtTheTime.toLocaleString()}</span>
             <span>원</span>
           </p>
         </section>
-      </section>
+      </section>`;
+        })
+        .join("")}
+
     </article>`;
     })
     .join("");
