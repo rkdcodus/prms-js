26972e2e4a885c127b1bb63914d9552ed376cb6d
rkdcodus <rcd7275@naver.com>
2025-01-06 22:51:22 +0900

    소비내역 삭제하기

--- js/store.js
+++ js/store.js
@@ -65,17 +65,17 @@ export function addNewHistory(newHistory) {
 
 export function removeHistory(dateId, itemId) {
   try {
-    // TODO:
-    /**
-     * - store의 detailList 새로 갱신
-     * - store.currentFunds 새로 갱신
-     */
-    store.detailList[dateId] = null;
+    store.detailList[dateId] = store.detailList[dateId].filter(({ id, amount }) => {
+      if (id === Number(itemId)) {
+        store.currentFunds += amount;
+      }
+      return id !== Number(itemId);
+    });
 
     updateStorage();
     return true;
   } catch (error) {
-    alert(error);
+    console.log(error);
     return false;
   }
 }
